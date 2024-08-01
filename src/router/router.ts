b182780269b2dd0e5800f0f7cbb416e548a0a6b9
NIHALELMAKHLOUFI <nihalelmakhloufi2@gmail.com>
2024-08-01 12:04:14 +0200

    fix route issue

---
+++ src/router/router.ts
@@ -0,0 +1,18 @@
+const router [
+    {
+        path: '/',
+        name "Home",
+        component: HomeFeed
+    },
+
+    {
+        path: '/video/:id"',
+        name "Home",
+        component: HomeFeed
+    },
+    {
+        path: '/video/:id"',
+        name "Home",
+        component: HomeFeed
+    },
+]
